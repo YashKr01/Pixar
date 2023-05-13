134346cb91963ba26fa2cfb96ac91d0fed428830
YashKr01 <yashkr2099@gmsil.com>
2023-05-13 22:22:01 +0530

    API Response

---
+++ app/src/main/java/com/techk/pixar/data/network/UnsplashApiResponse.kt
@@ -0,0 +1,28 @@
+package com.techk.pixar.data.network
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UnsplashApiResponseItem(
+    @SerializedName("id") val id: String?,
+    @SerializedName("likes") val likes: Int?,
+    @SerializedName("urls") val urls: Urls?,
+    @SerializedName("user") val user: User?,
+) : Parcelable
+
+@Parcelize
+data class Urls(
+    @SerializedName("full") val full: String?,
+    @SerializedName("raw") val raw: String?,
+    @SerializedName("regular") val regular: String?,
+    @SerializedName("small") val small: String?,
+) : Parcelable
+
+@Parcelize
+data class User(
+    @SerializedName("username") val username: String?
+) : Parcelable {
+    val attributionUrl get() = "https://unsplash.com/$username?utm_source=Pixar&utm_medium=referral"
+}
