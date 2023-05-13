4804a631929b79e690e5cddd8c0ad4616c8dd014
YashKr01 <yashkr2099@gmsil.com>
2023-05-13 20:15:46 +0530

    dimens.xml resources

--- app/src/main/java/com/techk/pixar/MainActivity.kt
+++ app/src/main/java/com/techk/pixar/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.techk.pixar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.techk.pixar.R
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        BuildConfig.API_KEY
 
     }
 
